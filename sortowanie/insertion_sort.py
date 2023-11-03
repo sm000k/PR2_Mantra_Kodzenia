fe9afd4154b188d22e9607848d101138b0651403
sm000k <pw.lacki@gmail.com>
2023-11-03 22:25:34 +0100

    insertion sort i bubble sort

--- sortowanie/insertion_sort.py
+++ sortowanie/insertion_sort.py
@@ -1,5 +1,19 @@
+import itertools
 
 
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        print (arr)
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            print(f"-->{key}...{arr}")
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
 
 if __name__ == "__main__":
-    pass
+
+    arr = [6, 5, 3, 2, 4,1]
+    insertion_sort(arr)
+    print("Sorted array:", arr)
